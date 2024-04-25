c6069f83eca393e7005bc7eafb08df9a5d6a0e15
arup-sarkar-24 <1arupsarkar112@gmail.com>
2024-04-25 11:37:14 +0530

    first commit

---
+++ src/main/java/com/mycompany/ContactServiceFindingSystem/ContactServiceFindingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.ContactServiceFindingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactServiceFindingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactServiceFindingSystemApplication.class, args);
+	}
+
+}
